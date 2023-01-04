11ded58c8805b54040dc3ecacc7a1073efce6043
nguyendung29822 <nguyenhuudung1276@gmail.com>
2023-01-04 10:14:39 +0700

    up

---
+++ module_2/src/ss16_text_file/doc_file/ReadCSV.java
@@ -0,0 +1,21 @@
+package ss16_text_file.doc_file;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+
+public class WriteCSV {
+    public static void main(String[] args) {
+        try{
+            File file = new File("src/ss16_text_file/doc_file/csv.data");
+            InputStream is = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(is);
+            Country country = (Country) ois.readObject();
+            System.out.println(country);
+            ois.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ thuchanh_code/src/taptin_thumuc/doc_object_tutaptin/DocDoiTuong.java
@@ -0,0 +1,2 @@
+package taptin_thumuc.doc_object_tutaptin;public class DocDoiTuong {
+}
